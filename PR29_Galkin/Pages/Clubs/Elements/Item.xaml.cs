70a46a412ee64dde1309f18001b40e7f670ee6d1
Nikita <galkinnikita228@gmail.com>
2024-06-10 15:55:45 +0500

    Разработанный модуль управления клубами. Galkin

---
+++ PR29_Galkin/Pages/Clubs/Elements/Item.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR29_Galkin.Pages.Clubs.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        Main Main;
+        Models.Clubs Club;
+        public Item(Models.Clubs Club, Main Main)
+        {
+            InitializeComponent();
+            if (Pages.Logins.LoginWin.IsAdmin == false) BthItem.Visibility = Visibility.Hidden;
+            this.Name.Text = Club.Name;
+            this.Address.Text = Club.Address;
+            this.WorkTime.Text = Club.WorkTime;
+            this.Main = Main;
+            this.Club = Club;
+        }
+
+        private void EditClub(object sender, RoutedEventArgs e) => MainWindow.init.OpenPages(new Pages.Clubs.Add(this.Main, this.Club));
+
+        private void DeleteClub(object sender, RoutedEventArgs e)
+        {
+            Main.AllClub.Clubs.Remove(Club);
+            Main.AllClub.SaveChanges();
+            Main.parent.Children.Remove(this);
+        }
+    }
+}
