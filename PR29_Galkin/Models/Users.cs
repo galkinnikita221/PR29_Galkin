6bf8edb273bbaa3c5d30edd2fa567c579cf2d704
Nikita <galkinnikita228@gmail.com>
2024-06-10 15:32:45 +0500

    Разработанные модели. Galkin

---
+++ PR29_Galkin/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PR29_Galkin.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public DateTime RentStart { get; set; }
+        public int Duration { get; set; }
+        public int IdClub { get; set; }
+        public int IdLogin { get; set; }
+    }
+}
